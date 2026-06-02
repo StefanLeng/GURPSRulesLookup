9b193e537762cda77aee8a139ed2c870db65601b
StefanLeng <stefan.leng@gmx.de>
2026-06-02 19:25:34 +0200

    filter by category

--- src/module/SearchSidebar.ts
+++ src/module/SearchSidebar.ts
@@ -24,12 +24,32 @@ export class SearchSidebar extends HandlebarsApplicationMixin(AbstractSidebarTab
     #rules: RulesEntry[] | null = null;
     #results: RulesEntry[] = [];
     #searchTerm: string = '';
+    #categories: { cat: string }[] = [];
+    #selectedCategories: string[] = [];
+    #message: string = 'To many results found. Please refine your search.';
 
     async loadRules() {
         try {
             const res = await fetch(`modules/GURPSRulesLookup/assets/gurps_rules.json`);
             if (!res.ok) throw new Error('HTTP ' + res.status);
-            this.#rules = await res.json();
+            this.#rules = await res.json().then((data) => {
+                return data.map((entry: any) => {
+                    return {
+                        ...entry,
+                        //normalize category to always be an array
+                        Category: Array.isArray(entry.Category)
+                            ? entry.Category.flatMap((cat: string) => cat.split(';').map((c: string) => c.trim()))
+                            : entry.Category.split(';').map((c: string) => c.trim()),
+                    };
+                });
+            });
+            if (this.#rules) {
+                this.#categories = Array.from(new Set(this.#rules.flatMap((r) => r.Category)))
+                    .sort()
+                    .map((cat) => {
+                        return { cat: cat };
+                    });
+            }
         } catch (err: any) {
             ui?.notifications?.error('GURPS Rules Lookup: Could not load Rules Database' + err.message);
             return;
@@ -58,8 +78,17 @@ export class SearchSidebar extends HandlebarsApplicationMixin(AbstractSidebarTab
     override async _prepareContext(
         options: foundry.applications.sidebar.Sidebar.RenderOptions,
     ): Promise<SearchSidebarRenderContext> {
+        if (!this.#rules) await this.loadRules();
         const SuperContext = await super._prepareContext(options);
-        const context = { ...SuperContext, results: this.#results, searchTerm: this.#searchTerm };
+        const context = {
+            ...SuperContext,
+            results: this.#results,
+            searchTerm: this.#searchTerm,
+            categories: this.#categories.map((c) => {
+                return { cat: c.cat, selected: this.#selectedCategories.includes(c.cat) };
+            }),
+            message: this.#message,
+        };
         return context;
     }
 
@@ -79,7 +108,6 @@ export class SearchSidebar extends HandlebarsApplicationMixin(AbstractSidebarTab
         options: DeepPartial<foundry.applications.sidebar.Sidebar.RenderOptions>,
     ): Promise<void> {
         super._onRender(context, options);
-        if (!this.#rules) await this.loadRules();
 
         const searchInput = this.element.querySelector<HTMLInputElement>('input[data-action="search"]');
         if (searchInput) {
@@ -94,15 +122,38 @@ export class SearchSidebar extends HandlebarsApplicationMixin(AbstractSidebarTab
                     this.render({ parts: ['results'] });
                     return;
                 }
-                if (!this.#rules) return;
-                this.#results = this.#rules.filter((r: RulesEntry) =>
-                    r.Rule.toLowerCase().includes(value.toLowerCase()),
-                );
-                if (this.#results.length > 35) {
-                    this.#results = [];
-                }
-                this.render({ parts: ['results'] });
+                this.search();
             });
         }
+        const categorySelect = this.element.querySelector<HTMLSelectElement>('multi-select[data-action="category"]');
+        if (categorySelect) {
+            categorySelect.addEventListener('change', (event) => {
+                event.preventDefault();
+                const target = event.currentTarget as HTMLSelectElement;
+                const values = Array.from(target.value);
+                if (JSON.stringify(values) === JSON.stringify(this.#selectedCategories)) return;
+                this.#selectedCategories = values;
+                this.search();
+            });
+        }
+    }
+
+    private search() {
+        if (!this.#rules) return;
+        this.#results = this.#rules.filter((r: RulesEntry) => {
+            return (
+                r.Rule.toLowerCase().includes(this.#searchTerm.toLowerCase()) &&
+                (this.#selectedCategories.length === 0 ||
+                    r.Category.some((cat) => this.#selectedCategories.includes(cat)))
+            );
+        });
+        if (this.#results.length === 0) {
+            this.#message = 'No results found.';
+        }
+        if (this.#results.length > 80) {
+            this.#results = [];
+            this.#message = 'To many results found. Please refine your search.';
+        }
+        this.render({ parts: ['results'] });
     }
 }
